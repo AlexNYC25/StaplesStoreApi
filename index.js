a61b79dacc5071a287e9fe36d488550b8aa9ec0c
Alexis Montes <montes.alexis25@gmail.com>
2020-07-03 22:25:10 -0400

    initial heroku files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+let app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
+app.get('/test', (req, res) => {
+    res.json({message: 'Hello World'})
+})
+
+app.listen(8080, () => {
+    console.log('Connected on port 8080')
+})
